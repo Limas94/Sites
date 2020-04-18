84c66bf97597c3ea5ad203d4814f6702034eb7aa
Limas94 <limas-m@yandex.ru>
2020-04-18 18:11:22 +1000

    Добавлена адаптивка

--- cloud-budget/src/global/index.js
+++ cloud-budget/src/global/index.js
@@ -1,6 +1,8 @@
 import "./scss/styles.scss"
 import menu from "../components/menu/menu";
 import slider from "../components/section-4/slider";
+import contact from "../components/contact/contact"
 
 menu();
 slider();
+contact();
