9eb89f3dcf81c4fe71811f28ac4d4d1fa46c3100
Limas94 <limas19942811@gmail.com>
2020-07-25 20:19:02 +1000

    Commit

--- CloudBudget/webpack.config.js
+++ CloudBudget/webpack.config.js
@@ -93,7 +93,7 @@ module.exports = {
 		}),
 
 		new CopyPlugin([
-			{ from: './src/assets/img/', to: './assets/' },
+			{ from: './src/assets/images/', to: './assets/' },
 		]),
 
 		new MiniCssExtractPlugin({
