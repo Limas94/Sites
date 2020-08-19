a7bda2e81dfaa448eb7465f23df18bb6700f9e5d
Limas94 <limas-m@yandex.ru>
2020-08-19 19:21:40 +1000

    Добавлена адаптивность слайдера

--- Blooger/js/main.js
+++ Blooger/js/main.js
@@ -1,3 +1,10 @@
+const responsive = {
+    0: { items: 1 },
+    320: { items: 1 },
+    560: { items: 2 },
+    960: { items: 3 }
+};
+
 $(document).ready(function() {
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
@@ -13,8 +20,9 @@ $(document).ready(function() {
         // autoplay: true,
         autoplayTimeout: 3000,
         dots: false,
-        nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        nav: true, 
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
 
     // Скролл
